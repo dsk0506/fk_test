682ee2277f61f2b6ea50c5752a2192e3a299a019
zhangpengcheng <zhangpengcheng@quancheng-ec.com>
2016-06-22 16:13:15 +0800

    mongo redis conf

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,11 +1,17 @@
 # coding:utf-8
 import requests
 import json
-
+from conf_fixture import mongo_cur
 
 def test_user():
     assert 1 == 1
 
 
-def test_mongo(redis_cur):
-    pass
+def test_mongo(mongo_cur):
+    '''
+    mongo connect test
+    :param mongo_cur:
+    :return:
+    '''
+    print mongo_cur.apply.find_one({'apply_id': 63926})
+    return
