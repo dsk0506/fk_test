bcd6b6a66f366268f5b0363590e122211fcd183b
zhangpengcheng <zhangpengcheng@quancheng-ec.com>
2016-07-05 17:18:32 +0800

    api count

--- bin/env.sh
+++ bin/env.sh
@@ -1,3 +1,5 @@
 source env/bin/activate
 export PYTHONPATH=`pwd`/src
 alias run="python `pwd`/src/test.py"
+alias api_count="cat `pwd`/src/tests/*.py|grep 'global_params.post'|awk -F \"'\" '{print \$2}' | sort |uniq -c |wc -l"
+
