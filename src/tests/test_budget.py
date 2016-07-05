99661a1c57dfbd0a82280a74b5f69adff7154cb8
zhangpengcheng <zhangpengcheng@quancheng-ec.com>
2016-07-05 10:52:41 +0800

    budget

---
+++ src/tests/test_budget.py
@@ -0,0 +1,18 @@
+# -*- coding:UTF-8-*-
+
+
+def test_budget_create():
+    pass
+
+
+def test_budget_list():
+    pass
+
+
+def test_budget_detail():
+    pass
+
+
+def test_budget_particular():
+    pass
+
